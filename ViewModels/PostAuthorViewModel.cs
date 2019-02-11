05247a3f66d65ebaf8dd9632ede8c2d30bca812f
James Naylor <jamesnaylor85@gmail.com>
2019-02-11 13:08:22 +0000

    Added post about WIP

---
+++ ViewModels/PostAuthorViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Deferat.Models;
+
+namespace Deferat.ViewModels
+{
+    public class PostAuthorViewModel
+    {
+        public Post Post { get; set; }
+        public Author Author { get; set; }
+    }
+}
---
+++ Views/Shared/Components/PostDetails/PostDetailsViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Deferat.Models;
+using Deferat.Services;
+using Deferat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Deferat.Components
+{
+    public class PostDetailsViewComponent : ViewComponent
+    {
+        private IAuthorService _authorService;
+
+        public PostDetailsViewComponent(IAuthorService authorService)
+        {
+            _authorService = authorService;
+        }
+
+        public IViewComponentResult Invoke(Post post)
+        {
+            var author = _authorService.GetAuthor(post.Author);
+
+            var viewModel = new PostAuthorViewModel()
+            {
+                Post = post,
+                Author = author
+            };
+
+            return View("Default", viewModel);
+            
+        }
+    }
+}
